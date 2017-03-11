d3fba248ae7781f7086cc47611d87b516db6ccd0
ElJerry <kanegerardo_@hotmail.com>
2017-03-10 23:23:33 -0600

    Modificacion a servicios web en php

--- Servicios PHP/cliente.php
+++ Servicios PHP/cliente.php
@@ -1,44 +1,25 @@
 <?php
 
         // require_once("nusoap/lib/nusoap.php");
-
+        
+        //borrar hasta aqui
         $servicio = 'http://localhost/gym/servicio.php?wsdl'; //url del servicio
         
 
 
-        $parametros = array();
+        $parametros = array('id'=>'1','nombre'=>'UAA2');
         
         $client = new SoapClient($servicio);
 
-        $result = $client->saludar($parametros);
+        $result = $client->registrarUniversidad($parametros);
 
-        echo $result;
+        echo $result;        
 
-        echo "asdads";
-        
-        
-        // $result = (array)$result;
-        // echo $result['return'];
-        // $n = count($tamano);
-        // //print_r($result);
-        // echo '<br>';
-        // echo 'Contenido de la respuesta: ' .$result['return'].'<br>';
-        // echo 'Tamaño del array: '.$n;
-        
-        // function obj2array($obj) {
-        //     $out = array();
-        //     foreach ($obj as $key => $val) {
-        //         switch (true) {
-        //             case is_object($val):
-        //                 $out[$key] = obj2array($val);
-        //                 break;
-        //             case is_array($val):
-        //                 $out[$key] = obj2array($val);
-        //                 break;
-        //             default:
-        //                 $out[$key] = $val;
-        //         }
-        //     }
-        //     return $out;
-        // }
-        ?>
+        // include("conectorsql.php");
+
+        // $sql = "insert into universidades(nombre) values ('UAA')";
+        // echo $sql;
+
+        // $conn ->query($sql);
+
+?>      
--- Servicios PHP/servicio.php
+++ Servicios PHP/servicio.php
@@ -1,27 +1,48 @@
 <?php
 	
 	require_once("nusoap/lib/nusoap.php");
+	
 
 	// Crear instancia del servidor
 	$server = new nusoap_server();
 	$server->configureWSDL("Web Service Gym","urn:wsGym");
 
+	//DEFINICIONES DE TIPOS DE DATOS
+	
+	// definicion de universidad
+	$server->wsdl->addComplexType('universidad','complexType','struct','all','',
+		array('id' => array('name'=>'ID','type'=>'xds:int')
+		, 'nombre' => array('name'=>'nombre', 'type'=>'xsd:string')));
 
-	$server->register('saludar',array(),
-            array('return' => 'xsd:string'),
-            'urn:wsGym',
-            'urn:wsGym#saludar',
-            'rpc',
-            'encoded',
-            'Te saludara');
+	
+	// Metodos!!
 
+	$server->register('registrarUniversidad',
+		array('universidad'=>'tns:universidad'),
+		array('return'=>'xsd:string'),
+		'urn:wsGym',
+        'urn:wsGym#registrarUniversidad',
+        'rpc',
+        'encoded',
+        'Agrega una universidad'
+		);
+
+	
+
+	
+
+	// montar el WS
 	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
     $server->service($HTTP_RAW_POST_DATA);
 
-    function saludar($in){
-    	// global $server;
-    	$saludo = array('return'=>'Hola weon');
 
-    	return "que pedo!";
+    //Funciones
+    function registrarUniversidad($uni){
+    	include('conectorsql.php');
+    	$sql = "insert into universidades (nombre) values('" . $uni['nombre'] . "')";
+    	$conn->query($sql);
+    	return $sql;
     }
+
+
 ?>
