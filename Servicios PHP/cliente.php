9d14e0c556cc4b5f250e0bc0e61e119e01493d2a
ElJerry <kanegerardo_@hotmail.com>
2017-03-11 16:18:25 -0600

    Agrego servicios web PHP junto con ejemplo de uso

--- Servicios PHP/cliente.php
+++ Servicios PHP/cliente.php
@@ -1,30 +1,46 @@
 <?php
 
-        // require_once("nusoap/lib/nusoap.php");
-        
-        //borrar hasta aqui
-        $servicio = 'http://localhost/gym/servicio.php?wsdl'; //url del servicio
+        //url del servicio
+        $servicio = 'http://localhost/gym/servicio.php?wsdl'; 
+        $client = new SoapClient($servicio);
+
         
 
-
+        // Ejemplo registrar recibo
         $parametros = array(
                 'id_recibo'=>'1',
-                'id_maestro'=>'2',
+                'id_alumno'=>'2',
                 'cantidad'=>'3',
                 'fecha'=>'enero 3',
                 );
-        
+
+        $result = $client->registrarReciboEntrante($parametros);
+
+        //Ejemplo getReciboEntrante
+
+        $parametros = '1';
+        $result = $client->getReciboEntrante($parametros);
+        $result = get_object_vars($result);
+        echo '<br>ReciboE: '.$result['Id_reciboE'].' '.$result['Cantidad'];
+
+        //Ejemplo getReciboSaliente
+
+        $parametros = '3';
+        $result = $client->getReciboSaliente($parametros);
+        $result = get_object_vars($result);
+        echo '<br>ReciboS: '.$result['Id_reciboS'].' '.$result['Cantidad'];
+
+        // Ejemplo registrarUniversidad
+        $parametros = array();
+        $parametros['id'] = 3; // no importa para registrar
+        $parametros['nombre'] = 'unam';
+        $result = $client->registrarUniversidad($parametros);
+
+        // Ejemplo getUniversidad;
+        $parametros = '1'; // Id universidad
         $client = new SoapClient($servicio);
-
-        $result = $client->registrarReciboSaliente($parametros);
-
-        echo $result;        
-
-        // include("conectorsql.php");
-
-        // $sql = "insert into universidades(nombre) values ('UAA')";
-        // echo $sql;
-
-        // $conn ->query($sql);
+        $result = $client->getUniversidad($parametros);
+        $result = get_object_vars($result);
+        echo '<br>Get uni: '.$result['id'].'-',$result['nombre'];   
 
 ?>      
--- Servicios PHP/servicio.php
+++ Servicios PHP/servicio.php
@@ -11,8 +11,9 @@
 
 	// definicion de universidad
 	$server->wsdl->addComplexType('universidad','complexType','struct','all','',
-		array('id' => array('name'=>'ID','type'=>'xds:int')
+		array('id' => array('name'=>'id','type'=>'xds:int')
 		, 'nombre' => array('name'=>'nombre', 'type'=>'xsd:string')));
+
 
 	//definicion de recibo entrante
 	$server->wsdl->addComplexType('reciboEntrante','complexType','struct','all','',array(
@@ -43,10 +44,32 @@
         'Agrega una universidad'
 		);
 
+	//get universidad
+	$server->register('getUniversidad',
+		array('id'=>'xsd:int'),
+		array('return'=>'xsd:Array'),
+		'urn:wsGym',
+        'urn:wsGym#getUniversidad',
+        'rpc',
+        'encoded',
+        'obtiene una universidad por id'
+		);
+
 	//registrar reciboEntrante
 	$server->register('registrarReciboEntrante',
 		array('recibo'=>'tns:reciboEntrante'),
 		array('return'=>'xsd:string'),
+		'urn:wsGym',
+        'urn:wsGym#registrarReciboEntrante',
+        'rpc',
+        'encoded',
+        'Agrega una universidad'
+		);
+
+	//get reciboEntrante
+	$server->register('getReciboEntrante',
+		array('recibo'=>'xsd:int'),
+		array('return'=>'xsd:Array'),
 		'urn:wsGym',
         'urn:wsGym#registrarReciboEntrante',
         'rpc',
@@ -65,6 +88,16 @@
         'Agrega una universidad'
 		);
 	
+	//get reciboSaliente
+	$server->register('getReciboSaliente',
+		array('recibo'=>'xsd:int'),
+		array('return'=>'xsd:Array'),
+		'urn:wsGym',
+        'urn:wsGym#registrarReciboSaliente',
+        'rpc',
+        'encoded',
+        'Agrega una universidad'
+		);
 
 	// montar el WS
 	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
@@ -79,6 +112,28 @@
     	return $sql;
     }
 
+    function getUniversidad($entrada){
+    	include('conectorsql.php');
+    	$sql = "select * from universidades where id_universidad='".$entrada."'";
+    	// return $sql;
+    	$id = 0;
+    	$nom = '0';
+
+    	if($res = $conn->query($sql)){
+    		$obj = $res->fetch_assoc();
+    		$id = $obj['Id_universidad'];
+    		$nom = $obj['Nombre'];
+    		// return $obj;
+    	}
+    	// else
+    		// return "neee";
+
+		$solo['id'] = $id;
+		$solo['nombre'] = $nom;
+
+    	return $solo;
+    }
+
     function registrarReciboEntrante($recibo){
     	include('conectorsql.php');
     	$idr = $recibo['id_recibo'];
@@ -88,10 +143,27 @@
     	$sql = "insert into recibos_entrante (id_alumno,cantidad,fecha) values("
     			.$ida.",".$cant.",'".$fecha."')";
 
-    	if($conn->query($sql))
+    	if($conn->query($sql)){
     		return $sql;
+    	}
 
     	return 'nel pastel';
+    }
+
+    function getReciboEntrante($id){
+    	include('conectorsql.php');
+    	$sql = "Select * from recibos_entrante where Id_reciboE=".$id;
+    	$recibo = array();
+
+    	if($res = $conn->query($sql)){
+    		$obj = $res->fetch_assoc();
+    		$recibo['Id_reciboE'] = $obj['Id_reciboE'];
+    		$recibo['Id_alumno'] = $obj['Id_alumno'];
+    		$recibo['Cantidad'] = $obj['Cantidad'];
+    		$recibo['Fecha'] = $obj['Fecha'];
+    	}
+
+    	return $recibo;
     }
 
     function registrarReciboSaliente($recibo){
@@ -107,5 +179,21 @@
     		return $sql;
 
     	return 'nel pastel';
+    }
+
+    function getReciboSaliente($id){
+    	include('conectorsql.php');
+    	$sql = "Select * from recibos_saliente where Id_reciboS=".$id;
+    	$recibo = array();
+
+    	if($res = $conn->query($sql)){
+    		$obj = $res->fetch_assoc();
+    		$recibo['Id_reciboS'] = $obj['Id_reciboS'];
+    		$recibo['Id_maestro'] = $obj['Id_maestro'];
+    		$recibo['Cantidad'] = $obj['Cantidad'];
+    		$recibo['Fecha'] = $obj['Fecha'];
+    	}
+
+    	return $recibo;
     }
 ?>
